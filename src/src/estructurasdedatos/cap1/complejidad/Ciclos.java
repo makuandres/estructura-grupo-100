3d93178a674739a6ba2dcfb677f35fe716ccb6f2
makuandres <30761116+makuandres@users.noreply.github.com>
2017-09-01 10:55:32 -0500

    Update Ciclos.java

--- src/src/estructurasdedatos/cap1/complejidad/Ciclos.java
+++ src/src/estructurasdedatos/cap1/complejidad/Ciclos.java
@@ -7,39 +7,59 @@ package estructurasdedatos.cap1.complejidad;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author tusk
+ */
 public class Ciclos {
     
-    public void ejecutarejercicio1() {
-    //ejercicio 19 
-        System.out.println("ejercicios 1 ciclos ");
-        System.out.println("19.  Que muestre la tabla de multiplicar de un número cualquiera ");
-        int numero = 0;
-        System.out.println("Introduzca un numero: ");
-        Scanner n=new Scanner(System.in);
-        numero=n.nextInt();
-     
-        for(int x = 1; x <= 10; x++) 
-        { System.out.println(numero +" * " + x + " = "+ numero*x); 
-        } }
-    
-    public void ejecutarejercicio2(){ 
-        System.out.println("ejercicios 2 ciclos ");
-        System.out.println("24.  Que calcule la suma de los cuadrados de los 100 primeros números "); 
-        int suma = 0; 
-        for(int i = 0; i < 50; i++) { 
-        suma = suma + i * i; } 
-        { System.out.println("la suma de los cuadrados de los 100 primeros números es:"+suma); 
-                }}
-
-    public void ejecutarejercicio3(){
-            System.out.println("ejercicios 3 ciclos ");
+    public void ejecutarejerciciosciclos() {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int respuesta;
+              
+        System.out.println("Menu ejercicio Ciclos");
+        System.out.println("1.  Que muestre la tabla de multiplicar de un número cualquiera ");
+        System.out.println("2.  Que calcule la suma de los cuadrados de los 100 primeros números ");
+        System.out.println("3.  Que calcule la media de X números, se dejarán de solicitar números hasta que se introduzca el cero ");
+        System.out.println("4. Que calcule el factorial de un número cualquiera y lo muestre en pantalla. ");   
+        
+        respuesta = entrada.nextInt();
+        
+        switch(respuesta){
+            
+        case 1:
+            
+            System.out.println("ejercicios 1 ciclos ");
+            int numero = 0;
+            System.out.println("Introduzca un numero: ");
             Scanner n=new Scanner(System.in);
-            System.out.println("23.  Que calcule la media de X números, se dejarán de solicitar números hasta que se introduzca el cero ");
+            numero=n.nextInt();
+     
+            for(int x = 1; x <= 10; x++) 
+            { System.out.println(numero +" * " + x + " = "+ numero*x); 
+            }
+            break;
+        
+        case 2:
+    
+            System.out.println("ejercicios 2 ciclos ");
+            int suma = 0; 
+            for(int i = 0; i < 50; i++) { 
+            suma = suma + i * i; } 
+            { System.out.println("la suma de los cuadrados de los 100 primeros números es:"+suma); 
+                }
+            break;
+
+        case 3:
+            System.out.println("ejercicios 3 ciclos ");
+            System.out.println();
             int num,suma2,elementos;
             float media;
-            
+            Scanner m=new Scanner(System.in);
             System.out.println("Introduzca un numero: ");
-            num=n.nextInt();
+            num=m.nextInt();
        
             suma2=0;
             elementos=0;
@@ -50,7 +70,7 @@ public class Ciclos {
             elementos++;
            
             System.out.println("Introduzca otro numero: ");
-            num=n.nextInt();
+            num=m.nextInt();
             }
             if(elementos==0){
            
@@ -60,19 +80,24 @@ public class Ciclos {
             else{
            media=(float)suma2/elementos;
            System.out.println("La media es de: "+media);
-            }}
+            }
+            break;
 
-    public void ejecutarejercicio4(){
-        System.out.println("ejercicios 4 ciclos ");
-        System.out.println("21. Que calcule el factorial de un número cualquiera y lo muestre en pantalla. ");
-        Scanner leer=new Scanner(System.in);
-        System.out.println("Introduzca un numero: ");
-        int numero3=leer.nextInt();
-        int factorial=1;
-        while(numero3 !=0)
-        {   factorial*=numero3;
+        case 4:
+            System.out.println("ejercicios 4 ciclos ");
+            System.out.println();
+            Scanner leer=new Scanner(System.in);
+            System.out.println("Introduzca un numero: ");
+            int numero3=leer.nextInt();
+            int factorial=1;
+            while(numero3 !=0)
+            {   factorial*=numero3;
             numero3 --;
+            }
+            System.out.println("el factorial es: "+ factorial);
+            
+            break;
         }
-        System.out.println("el factorial es: "+ factorial);}
-    
+}
+
 }
